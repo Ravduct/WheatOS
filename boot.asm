aa26ac43cb411cdd2e63430c98520993eab8c7b7
Ravduct <ravduct1701@gmail.com>
2026-06-09 22:34:04 -0400

    First update

---
+++ boot.asm
@@ -0,0 +1,24 @@
+[org 0x7C00]
+[bits 16]
+
+start:
+    mov si, message
+
+print_loop:
+    lodsb
+    cmp al, 0
+    je hang
+
+    mov ah, 0x0E
+    int 0x10
+
+    jmp print_loop
+
+hang:
+    jmp $
+
+message:
+    db 'Hello, World!', 0
+
+times 510-($-$$) db 0
+dw 0xAA55

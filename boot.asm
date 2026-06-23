0b5e3083d3bc1883a4b9d2da55dc3632409995aa
Ravduct <ravduct1701@gmail.com>
2026-06-23 14:59:49 -0400

    Stage 1 bootloader

--- boot.asm
+++ boot.asm
@@ -2,17 +2,20 @@
 [bits 16]
 
 _start:
-    mov si, switching_32bit_message
-    call print_loop
     cli
-    call enable_A20_gate
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
 
-    jump hang
+    mov [boot_drive], dl
 
+    mov si, switching_stage2_message
+    call print_loop
 
-enable_A20_gate:
-    mov ax, 0x2401
-    int 0x15
+    jmp hang
 
 print_loop:
     lodsb
@@ -26,9 +29,10 @@ print_loop:
 
 hang:
     hlt
-    jump hang
+    jmp hang
 
-switching_32bit_message: db 'Loading into Protected Mode 32-bit', 0
+switching_stage2_message: db 'Loading into Stage 2 bootloader', 0
+boot_drive: db 0
 
 times 510-($-$$) db 0
 db 0x55, 0xAA

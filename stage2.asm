871db88b71d0f72d0844147391de95f635eed943
Ravduct <ravduct1701@gmail.com>
2026-07-21 00:11:29 -0400

    7/21/2026 12:11am

--- stage2.asm
+++ stage2.asm
@@ -10,7 +10,36 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7C00
 
-check_a20:
+.enable_a20:
+    mov     ax, 0x2403
+    int     0x15
+    call check_a20
+    cmp byte [a20_status], 1
+    je .exit
+
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    cmp byte [a20_status], 1
+    je .exit
+
+    cli
+    
+    mov al, 0xAD
+    out 0x64, al
+    .wait_kbc_input:
+        in 0x64, al
+        test al, 0x02
+        jnz .wait_kbc_input
+    mov al, 0xD0
+    mov 0x64, al
+    
+
+
+.exit:
+    ret
+
+.check_a20:
     pushf
     push ds
     push es
@@ -40,6 +69,7 @@ check_a20:
     jne .a20_is_on
 
     mov byte [a20_status], 0
+    sti
     pop ax
     pop si
     pop di
@@ -47,8 +77,10 @@ check_a20:
     pop ds
     popf
     ret
+
     .a20_is_on:
         mov byte [a20_status], 1
+        sti
         pop ax
         pop si
         pop di
@@ -56,9 +88,5 @@ check_a20:
         pop ds
         popf
         ret
-
-.enable_a20:
-    ;code 
-
 
 a20_status: db 0

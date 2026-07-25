cea0b7a46886d55d5a2206c5f3e0aca0dcd01b66
Ravduct <ravduct1701@gmail.com>
2026-07-25 01:13:11 -0400

    e820 memory map completed

--- stage2.asm
+++ stage2.asm
@@ -13,11 +13,56 @@ stage2_main:
     call enable_a20
     mov si, a20_ready
     call print_string
+    call e820_memory_map
 
     jmp halt
 
 %include "disk.asm"
 %include "print.asm"
+
+e820_memory_map:
+    push eax
+    push edx
+    push ecx
+    push es
+    push di
+    push ebx
+
+    xor ax, ax
+    xor ebx, ebx
+    mov es, ax
+    mov di, e820_buffer
+    e820_loop:
+        mov eax, 0xE820
+        mov edx, 0x534D4150
+        mov ecx, 24
+        int 0x15
+        jc .e820_error
+
+        cmp eax, 0x534D4150
+        jne .e820_error
+
+        inc byte [entry_count]
+        add di, 24
+
+        cmp ebx, 0
+        jne e820_loop
+        je .e820_exit
+    .e820_exit:
+        pop ebx
+        pop di
+        pop es
+        pop ecx
+        pop edx
+        pop eax
+        mov si, e820_finish_msg
+        call print_string
+        ret
+
+    .e820_error:
+        mov si, e820_error_msg
+        call print_string
+        jmp $
 
 enable_a20:
     push ax
@@ -27,14 +72,14 @@ enable_a20:
     int 0x15
     call check_a20
     cmp byte [a20_status], 1
-    je .exit
+    je .a20_exit
 
     in al, 0x92
     or al, 2
     out 0x92, al
     call check_a20
     cmp byte [a20_status], 1
-    je .exit
+    je .a20_exit
 
     cli
     
@@ -71,11 +116,11 @@ enable_a20:
     sti
     call check_a20
     cmp byte [a20_status], 1
-    je .exit
+    je .a20_exit
 
     jmp halt
 
-.exit:
+.a20_exit:
     pop bx
     pop ax
     ret
@@ -141,6 +186,10 @@ check_a20:
         ret
 
 a20_status: db 0
+entry_count: db 0
 stage2_begin_msg db "stage 2 ready", 0
 a20_ready db "a20 enabled", 0
 stage2_exit_msg db "stage 2 exit", 0
+e820_error_msg db "error at e820", 0
+e820_finish_msg db "e820 finished", 0
+e820_buffer: times 768 db 0

9ebfe819f4a8728dc55ce2ac6971bee4ffa988bb
Ravduct <ravduct@DESKTOP-1I4CQTT.localdomain>
2026-09-19 09:25:04 -0400

    trying to output words in kernel

--- kernel.c
+++ kernel.c
@@ -1,5 +1,10 @@
 void kernel_main(void *e820_map, int entry_count) {
-    while (1) {
-        
-    }
+
+
+    while (1) {}
+}
+
+void write_character(unsigned char c, unsigned char forecolor, unsigned char backcolor, int) {
+    uint16_t attribute = (backcolor << 4) | (forecolor & 0x0F);
+
 }
